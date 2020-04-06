1ebd02cf5b97d40111843534ab45be1b3250315b
IYarosh <IYarosh@qulix.com>
2020-04-06 18:01:19 +0300

    Trying to download from systemjs

--- src/single-spa-config.js
+++ src/single-spa-config.js
@@ -1,1 +1,10 @@
-console.log('Hi');
+import * as singleSpa from "single-spa";
+
+
+// singleSpa.registerApplication(
+//     "microfrontend-react-sspa-app1",
+//     () => System.import("microfrontend-react-sspa/app1"),
+//     () => true
+//   );
+  
+// singleSpa.start();
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,19 @@ module.exports = {
     entry: path.resolve(__dirname, "src/single-spa-config.js"),
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename: "bundle.js"
     },
     mode: "development",
     plugins: [
         new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })
     ],
     devServer: {
-        contentBase: path.resolve(__dirname, 'src/single-spa-config.js')
-    }
+        contentBase: path.resolve(__dirname, 'dist'),
+        historyApiFallback: true,
+        disableHostCheck: true,
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+    },
+    externals: ["microfrontend-react-sspa", /^microfrontend-react-sspa\/.+$/]
 }
